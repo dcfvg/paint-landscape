de3a439bfea37739e85a992d5231f423e58b09a2
dcfvg <qwsx@dcfvg.com>
2014-02-02 14:02:38 +0100

    clean version

--- paint-landscape.sh
+++ paint-landscape.sh
@@ -1,24 +1,71 @@
+#!/bin/bash
 
-setname="In_the_Conservatory"
-tiles=$setname/tiles/
+assets="assets"
+url="$assets/url"	# url list
+result="$assets/result"
 
-mkdir $setname
-cd $setname
 
-wget -nc -O 00.jpg http://upload.wikimedia.org/wikipedia/commons/d/d5/In_the_Conservatory-x0-y0.jpg
-wget -nc -O 01.jpg http://upload.wikimedia.org/wikipedia/commons/e/e4/In_the_Conservatory-x1-y0.jpg
-wget -nc -O 02.jpg http://upload.wikimedia.org/wikipedia/commons/9/9d/In_the_Conservatory-x2-y0.jpg
-wget -nc -O 03.jpg http://upload.wikimedia.org/wikipedia/commons/a/a7/In_the_Conservatory-x0-y1.jpg
-wget -nc -O 04.jpg http://upload.wikimedia.org/wikipedia/commons/2/24/In_the_Conservatory-x1-y1.jpg
-wget -nc -O 05.jpg http://upload.wikimedia.org/wikipedia/commons/2/29/In_the_Conservatory-x2-y1.jpg
-wget -nc -O 06.jpg http://upload.wikimedia.org/wikipedia/commons/a/a4/In_the_Conservatory-x0-y2.jpg
-wget -nc -O 07.jpg http://upload.wikimedia.org/wikipedia/commons/a/aa/In_the_Conservatory-x1-y2.jpg
-wget -nc -O 08.jpg http://upload.wikimedia.org/wikipedia/commons/3/33/In_the_Conservatory-x2-y2.jpg
+mkdir -v $assets $result $url
+resolutions=("1280x960" "1920x1080")
 
-# gm montage -monitor -geometry +0+0 -tile 3x3 *.jpg final.miff
 
-mkdir tiles
-#gm convert final.miff -crop 1280x960 +adjoin tiles/tile-%05d.jpg
 
-cd tiles
-ffmpeg -f image2 -pattern_type glob -i '*.jpg' -r 25 -vcodec mpeg4 -b 30000k -vf scale=1280:-1 -y all.mp4
+
+if [[ $# -eq 0 ]]
+then
+	echo 'warning ! no url list'
+	exit 0
+fi
+
+urlpath=$1
+urlfile=$(basename $urlpath)
+setname="${urlfile%.*}"
+
+function initproject {
+
+	setpath="$result/$setname"
+	
+	raw="$setpath/raw"	# orignal images
+	tmp="$setpath/tmp"	# tmp folder
+	mov="$setpath/mov"	# result as mp4
+	stk="$setpath/stk"	# result as tiles
+
+	mkdir -v $setpath $tmp $mov $stk $raw
+}
+function gettilesHD {
+	i=0
+	for file in $(cat $urlpath)
+	do	
+		id=`printf %02d $i`
+		echo "wget -nc -O $id.jpg $file"
+		((i++))
+		
+		x=${file: -5 :1}
+		y=${file: -8 :1}
+		
+	done
+	tilesetsize="$(($x + 1))x$(($y + 1))"
+	echo "tilesetsize $tilesetsize"
+}
+function tilemontage {
+	echo gm montage -monitor -geometry +0+0 -tile $tilesetsize "$raw/*.jpg" "$tmp/$setname.miff"
+}
+function stack {
+	for res in ${resolutions[@]} 
+	do
+		tilepath="$stk/tiles-$res"
+		mkdir $tilepath
+		gm convert "$tmp/$setname.miff" -crop $res +adjoin $tilepath/%05d.jpg
+		
+		ffmpeg -f image2 -pattern_type glob -i "$tilepath/*.jpg" \
+		-r 25 -vcodec mpeg4 -b 30000k -vf \
+		-y $mov/setname-$res.mp4
+	done
+}
+
+echo "starting $setname"
+
+initproject		# create folder tree
+gettilesHD		# get tiles from url
+tilemontage		# assemble all tiles in one .miff image
+stack			# create tiles from the .miff image and compile them into a movie
