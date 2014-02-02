7b5dfc952b6e80df6b61a928a5ca77d43d00637e
dcfvg <qwsx@dcfvg.com>
2014-02-02 14:29:14 +0100

    read all .md in directory in search for url

--- paint-landscape.sh
+++ paint-landscape.sh
@@ -2,7 +2,7 @@
 
 if [[ $# -eq 0 ]] # check if a param is entered
 then
-	echo 'warning ! no url list'
+	echo 'warning ! no url list path set'
 	exit 0
 fi
 
@@ -29,7 +29,7 @@ function gettilesHD {
 	for file in $(cat $urlpath)
 	do	
 		id=`printf %02d $i`
-		wget -nc -O "$raw/$id.jpg" $file
+		echo wget -nc -O "$raw/$id.jpg" $file
 		((i++))
 		
 		x=${file: -5 :1}
@@ -40,7 +40,7 @@ function gettilesHD {
 	printf "tiles: $i \t tilesetsize: $tilesetsize"
 }
 function tilemontage {
-	gm montage -monitor -geometry +0+0 -tile $tilesetsize "$raw/*.jpg" "$tmp/$setname.miff"
+	echo gm montage -monitor -geometry +0+0 -tile $tilesetsize "$raw/*.jpg" "$tmp/$setname.miff"
 }
 function stack {
 	for res in ${resolutions[@]} 
@@ -49,25 +49,28 @@ function stack {
 		mkdir $tilepath
 		
 		echo "tiles @$res"
-		gm convert "$tmp/$setname.miff" -crop $res +adjoin $tilepath/%05d.jpg
+		echo gm convert "$tmp/$setname.miff" -crop $res +adjoin $tilepath/%05d.jpg
 		
 		echo "mov @$res"
-		ffmpeg -f image2 -pattern_type glob -i "$tilepath/*.jpg" \
+		echo ffmpeg -f image2 -pattern_type glob -i "$tilepath/*.jpg" \
 		-r 25 -vcodec mpeg4 -b 30000k -vf \
 		-y $mov/setname-$res.mp4
 	done
 }
 
-urlpath=$1
-urlfile=$(basename $urlpath)
-setname="${urlfile%.*}"
-
 resolutions=("1280x960" "1920x1080")
-echo "starting $setname"
 
-clear
-initenv			# create basic folder tree 
-initproject		# create folder tree
-gettilesHD		# get tiles from url
-tilemontage		# assemble all tiles in one .miff image
-stack			# create tiles from the .miff image and compile them into a movie
+for urlpath in `find $1 -iname "*.md" -type f`
+  do
+	echo "starting $setname"
+
+	urlfile=$(basename $urlpath)
+	setname="${urlfile%.*}"
+	
+    clear
+	initenv			# create basic folder tree 
+	initproject		# create folder tree
+	gettilesHD		# get tiles from url
+	tilemontage		# assemble all tiles in one .miff image
+	stack			# create tiles from the .miff image and compile them into a movie
+done
