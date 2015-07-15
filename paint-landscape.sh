e52d20f3cb3ed6c8e80b4017708d2f7ce139b6dd
bverjat <benoit.verjat@sciences-po.fr>
2015-07-15 15:08:53 +0200

    adding --no-check-certificate

--- paint-landscape.sh
+++ paint-landscape.sh
@@ -16,7 +16,7 @@ function initenv {
 function initproject {
 
 	setpath="$result/$setname"
-	
+
 	raw="$setpath/raw"	# orignal images
 	tmp="$setpath/tmp"	# tmp folder
 	mov="$setpath/mov"	# result as mp4
@@ -27,35 +27,35 @@ function initproject {
 function gettilesHD {
 	i=0
 	for file in $(cat $urlpath)
-	do	
+	do
 		id=`printf %02d $i`
-		wget -c -O "$raw/$id.jpg" $file
+		wget --no-check-certificate -c -O "$raw/$id.jpg" $file
 		((i++))
-		
+
 		x=${file: -5 :1}
 		y=${file: -8 :1}
-		
+
 	done
 	tilesetsize="$(($x + 1))x$(($y + 1))"
 	printf "tiles: $i \t tilesetsize: $tilesetsize"
 }
 function tilemontage {
-	tmpmontage="$tmp/$setname.miff"	
+	tmpmontage="$tmp/$setname.miff"
 	if [ ! -f tmpmontage ]
 	then
 		gm montage -monitor -geometry +0+0 -tile $tilesetsize "$raw/*.jpg" $tmpmontage
 	fi
 }
 function stack {
-	for res in ${resolutions[@]} 
+	for res in ${resolutions[@]}
 	do
 		tilepath="$stk/tiles-$res"
 		mkdir $tilepath
-		
+
 		echo "tiles @$res"
 		gm convert "$tmp/$setname.miff" -crop $res +adjoin $tilepath/%05d.jpg
 		gm mogrify -background black -extent $res $tilepath/*jpg
-		
+
 		echo "mov @$res"
 		ffmpeg -f image2 -pattern_type glob -i "$tilepath/*.jpg" \
 		-r 25 -vcodec mpeg4 -b 30000k \
@@ -63,22 +63,22 @@ function stack {
 	done
 }
 
-resolutions=("1280x960" "1920x1080" "1136x640")
- 
+resolutions=("1920x1080")
+
 for urlpath in `find $1 -iname "*.md" -type f`
 do
 	clear
-	
+
 	urlfile=$(basename $urlpath)
 	setname="${urlfile%.*}"
 	echo "starting $setname"
-	
-	initenv			  # create basic folder tree 
+
+	initenv			  # create basic folder tree
 	initproject		# create folder tree
 	gettilesHD		# get tiles from url
 	tilemontage		# assemble all tiles in one .miff image
 	stack			    # create tiles from the .miff image and compile them into a movie
-	
+
 	mv $urlpath "$setpath/$urlfile"  # move url list in current folder
 	rm $tmpmontage                   # delete cache file
 done
